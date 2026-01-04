7970790ce7a93e851b4e1ce013c5c548d2ec85a3
shivam-sky-57 <shivamkumar7965@gmail.com>
2026-01-04 21:33:50 +0530

    db schema added

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -10,6 +10,16 @@ app.use(clerkMiddleware()) //auth obj will be attached to the req
 app.use(express.json()); // parses JSON request bodies.
 app.use(express.urlencoded({ extended: true })); // parse form data (like HTML forms).
 
+app.get("/", (req, res) => {
+    res.json({
+        message:"Welcome to Productify API - Powered by PostgreSQL, Drizzle ORM & Clerk Auth",
+        endpoints: {
+            users: "/api/users",
+            products: "/api/products",
+            comments: "/api/comments",
+        }
+    })
+})
 
 
 app.listen(ENV.PORT, () => console.log("Server is up and running on PORT:",ENV.PORT));
